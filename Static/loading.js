041dcd353dd59557203d1c6bb8011e1be3f11666
Daniel Hernandez Cuero <dhernandez49@javerianacali.edu.co>
2019-06-25 01:35:47 -0500

    Carga de todo x seguridad

--- Static/loading.js
+++ Static/loading.js
@@ -7,6 +7,7 @@ $(document).ready(function() {
         $('#content').hide();
       });
       $('#opciones').click(function(){
+        $("#opciones").hide();
         $('div').show();
       });
     });
